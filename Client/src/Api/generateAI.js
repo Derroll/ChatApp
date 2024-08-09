f1abe72e13793ab3114026d1fad3f6d6f81a88b9
Derroll David <derroll.david@dbizsolution.com>
2024-08-09 16:44:36 +0530

    Included env file

--- Client/src/Api/generateAI.js
+++ Client/src/Api/generateAI.js
@@ -1,15 +1,14 @@
 import axios from "axios";
-import * as dotenv from "dotenv";
-dotenv.config();
 
 export const generateAIApi = async(currentMessage) => {
+    const url = import.meta.env.VITE_GENERATE_AI_API;
     const body = {
         input: currentMessage
     }
     try {
         const response = await axios({
             method: "post",
-            url: process.env.GENERATE_AI_API,
+            url,
             data: body
         });
         return response;
--- Client/src/Api/uploadPDF.js
+++ Client/src/Api/uploadPDF.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 import FormData from "form-data";
-import * as dotenv from "dotenv";
-dotenv.config();
 
 export const uploadPDF = async (selectedFile) => {
+    const url = import.meta.env.VITE_UPLOAD_API;
     if (selectedFile) {
         const form = new FormData();
         form.append('file', selectedFile);
         try {
             const response = await axios({
                 method: "post",
-                url: process.env.UPLOAD_API,
+                url,
                 data: form,
                 headers: {
                     'Content-Type': 'multipart/form-data'
